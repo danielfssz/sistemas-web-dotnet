d48f65f8f8bbf33b756759ec0d0d0db1eb39e1c8
Daniel Souza <danielfss98@gmail.com>
2019-02-22 20:30:04 -0300

    Arquivos iniciais aula3

---
+++ Aula3/Aula2/Conexao.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace Aula2
+{
+    class Conexao
+    {
+        SqlConnection conn = new SqlConnection("Data Source=localhost\\SQLEXPRESS;Initial Catalog=aula2;Integrated Security=SSPI");
+        SqlCommand cmd = null;
+        SqlDataAdapter da = new SqlDataAdapter();
+
+         
+        public void Abrir() {
+            conn.Open();
+        }
+
+        public void Fechar() {
+            conn.Close();
+        }
+
+
+        public SqlDataReader retornaDataReader(String sql) {
+
+            try
+            {
+                SqlDataReader dr; //Objeto que receberá os dados do banco de dados
+                SqlCommand cmd = new SqlCommand(); //Comando sql
+                cmd.Connection = conn;
+                cmd.CommandText = sql;
+                dr = cmd.ExecuteReader();
+
+                return dr;
+            }
+            catch (Exception ex) {
+                throw ex;
+            }
+ 
+        }
+
+        public DataSet retornaDataset (String sql)
+        {
+
+            try
+            {
+                DataSet ds=new DataSet(); //Objeto que receberá os dados do banco de dados
+                SqlCommand cmd = new SqlCommand(); //Comando sql
+                cmd.Connection = conn;
+                cmd.CommandText = sql;
+                SqlDataAdapter da = new SqlDataAdapter();
+                da.SelectCommand = cmd;
+
+                da.Fill(ds,"MinhaTabela");
+
+                return ds;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+    }
+}
+
---
+++ Aula3/Aula2/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Aula2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 1. Instancia a conexão(objeto SqlConnection)
+            SqlConnection conn = new SqlConnection("Data Source=localhost\\SQLEXPRESS;Initial Catalog=aula2;Integrated Security=SSPI");
+            //
+                 
+            // define um SqlDataReader nulo
+            SqlDataReader dr =null;
+
+            try
+            {
+                // 2. Abre a conexão
+                conn.Open();
+                // 3. Passa conexão para o objeto command
+                SqlCommand cmd = new SqlCommand(); //("select * from login", conn);
+                cmd.Connection = conn;
+                cmd.CommandText = "select * from login";
+
+                SqlDataAdapter da = new SqlDataAdapter();
+
+                
+                // 4. Usa conexão
+                // obtêm o resultado da consulta
+                dr = cmd.ExecuteReader();
+                // imprime o codigo do cliente para cada registro
+                while (dr.Read())
+                {
+                    Console.WriteLine(dr["login"].ToString() + dr["Nome"].ToString());
+                }
+            }
+            finally
+            {
+                // fecha o reader
+                if (dr != null)
+                {
+                    dr.Close();
+                }
+                // 5. Fecha a conexão
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Conexao cn = new Conexao();
+            try
+            {
+               
+
+                cn.Abrir();
+                SqlDataReader dr = cn.retornaDataReader("select * from login");
+                //cn.Fechar(); 
+                while (dr.Read())
+                {
+                    Console.WriteLine(dr["login"].ToString() + dr["Nome"].ToString());
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally {
+                cn.Fechar();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Conexao cn = new Conexao();
+            try
+            {
+
+
+                cn.Abrir();
+                DataSet ds;
+                ds = cn.retornaDataset("select * from login");
+                cn.Fechar();
+
+                if (ds.Tables[0].Rows.Count > 0) {
+
+                    Console.WriteLine(ds.Tables[0].Rows[0]["login"].ToString() + " - "+ ds.Tables[0].Rows[0]["nome"].ToString());
+                }
+
+
+
+
+               /* while (dr.Read())
+                {
+                    Console.WriteLine(dr["login"].ToString() + dr["Nome"].ToString());
+                }*/
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                cn.Fechar();
+            }
+
+        }
+    }
+}
