05f2c0b0f0f8e28410a5b7fa5ccd4597ef00cd93
Daniel Souza <danielfss98@gmail.com>
2019-03-15 19:40:59 -0300

    add script da aula

---
+++ Aula5/script.sql
@@ -0,0 +1,66 @@
+create table contatos (
+codigo int identity(1,1) primary key,
+nome varchar(100),
+email varchar(100),
+idade int
+)
+
+go
+CREATE PROCEDURE AtualizarDados 
+@codigo int,
+@nome varchar(100),
+@email varchar(100),
+@idade int
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	update contatos 
+	set nome=@nome,
+	email=@email,
+	idade=@idade
+	where codigo=@codigo
+			
+
+END
+GO
+
+
+cREATE PROCEDURE dbo.CarregarDados
+AS
+    select * from contatos
+
+go
+
+CREATE PROCEDURE dbo.DeletarDados
+(
+@codigo int
+)
+AS
+DELETE
+    FROM dbo.Contatos
+    WHERE  
+    codigo = @codigo
+
+	go
+
+CREATE PROCEDURE dbo.InserirDados
+(
+@nome nvarchar(50),
+@email nvarchar(100),
+@idade int
+)
+AS
+Insert into Contatos(nome,email,idade) values 
+(@nome,@email,@idade)
+
+go
+
+
+CREATE PROCEDURE dbo.getContato
+(
+    @codigo int
+)
+AS
+select * from Contatos Where codigo = @codigo
+
