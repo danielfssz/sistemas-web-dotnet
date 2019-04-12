0f2b679afb94445d2b3976100aecf16486dfec8a
Daniel Souza <danielfss98@gmail.com>
2019-04-12 20:06:14 -0300

    Adicionando Models

---
+++ TP01/TP01Crud/TP01Crud/Models/Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TP01Crud.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string NomeFantasia { get; set; }
+        public string RazaoSocial { get; set; }
+        public string CNPJ { get; set; }
+        public string Rua { get; set; }
+        public string CEP { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+    }
+}
