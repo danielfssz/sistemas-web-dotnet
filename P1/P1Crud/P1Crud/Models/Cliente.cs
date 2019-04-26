26bdec103bd2a6f50a0305aafc6e4385c4a8b863
Daniel Souza <danielfss98@gmail.com>
2019-04-26 08:21:11 -0300

    Add model cliente

---
+++ P1/P1Crud/P1Crud/Models/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace P1Crud.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Cpf { get; set; }
+        public string Rg { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Logradouro { get; set; }
+        public string Cep { get; set; }
+        public string Cidade { get; set; }
+        public string Bairro { get; set; }
+    }
+}
