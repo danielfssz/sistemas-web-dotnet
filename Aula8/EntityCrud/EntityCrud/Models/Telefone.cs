616ccba57a742c991f18f046fedd691bf2557e5e
Daniel Souza <danielfss98@gmail.com>
2019-05-03 20:35:56 -0300

    Add models

---
+++ Aula8/EntityCrud/EntityCrud/Models/Telefone.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EntityCrud.Models
+{
+    public class Telefone
+    {
+        public int Id { get; set; }
+        public int DDD { get; set; }
+        public string Numero { get; set; }
+        public TipoTelefone tipoTelefone { get; set; }
+    }
+}
