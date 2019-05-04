3851787c540abe9381f62e99163256c6dd082b25
Daniel Souza <danielfss98@gmail.com>
2019-05-03 21:59:15 -0300

    Add ClienteController e Views

---
+++ Aula8/EntityCrud/EntityCrud/Controllers/ClientesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EntityCrud.Models;
+
+namespace EntityCrud.Controllers
+{
+    public class ClientesController : Controller
+    {
+        private ConsultoriaContext db = new ConsultoriaContext();
+
+        // GET: Clientes
+        public ActionResult Index()
+        {
+            var clientes = db.Clientes.Include(c => c.Consultor);
+            return View(clientes.ToList());
+        }
+
+        // GET: Clientes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cliente cliente = db.Clientes.Find(id);
+            if (cliente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cliente);
+        }
+
+        // GET: Clientes/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdConsultor = new SelectList(db.Consutores, "Id", "Nome");
+            return View();
+        }
+
+        // POST: Clientes/Create
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nome,Email,IdConsultor")] Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Clientes.Add(cliente);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdConsultor = new SelectList(db.Consutores, "Id", "Nome", cliente.IdConsultor);
+            return View(cliente);
+        }
+
+        // GET: Clientes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cliente cliente = db.Clientes.Find(id);
+            if (cliente == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdConsultor = new SelectList(db.Consutores, "Id", "Nome", cliente.IdConsultor);
+            return View(cliente);
+        }
+
+        // POST: Clientes/Edit/5
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nome,Email,IdConsultor")] Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cliente).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdConsultor = new SelectList(db.Consutores, "Id", "Nome", cliente.IdConsultor);
+            return View(cliente);
+        }
+
+        // GET: Clientes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cliente cliente = db.Clientes.Find(id);
+            if (cliente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cliente);
+        }
+
+        // POST: Clientes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Cliente cliente = db.Clientes.Find(id);
+            db.Clientes.Remove(cliente);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
