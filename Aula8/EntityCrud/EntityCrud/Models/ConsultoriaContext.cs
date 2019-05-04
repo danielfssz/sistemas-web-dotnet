95a1c8e8561f99f57e4d209c4e874d756bc90f0c
Daniel Souza <danielfss98@gmail.com>
2019-05-03 21:08:02 -0300

    Add DbContext e ConnectionString

---
+++ Aula8/EntityCrud/EntityCrud/Models/ConsultoriaContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EntityCrud.Models
+{
+    public class ConsultoriaContext : DbContext
+    {
+        public DbSet<Consultor> Consutores { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Telefone> Telefones { get; set; }
+
+        public ConsultoriaContext() : base("Conexao")
+        {
+
+        }
+    }
+}
