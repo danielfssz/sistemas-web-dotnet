cb548b92e638ce96024c717f2d744f893cf3c9e9
Italo Carvalho <italo.998.carvalho@gmail.com>
2019-06-07 21:03:32 -0300

    Estrutura inicial da API da P2

---
+++ P2/API/API/API/Models/Consultor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace API.Models
+{
+    public class Consultor
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public String Nome { get; set; }
+    }
+}
---
+++ P2/API/API/API/Models/Telefone.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API.Models
+{
+    public class Telefone
+    {
+        public int Id { get; set; }
+        public int DDD { get; set; }
+        public String Numero { get; set; }
+        public TipoTelefone Tipo { get; set; }
+    }
+}
